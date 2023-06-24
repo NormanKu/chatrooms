e94b9572377cfe7f89f62214588a746bf2068cbb
NormanKu <kunorman4@gmail.com>
2023-06-24 15:50:40 +1000

    Create a chatroom

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,20 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "98ae5a0e-7df3-4f33-8a9a-b75874a7fa28",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
